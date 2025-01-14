37979bf4170a82ee47c10883d8cfb54c1f20a08f
Sasha Pimenov <pimenovs33@gmail.com>
2025-01-14 16:35:49 +0200

    Modified app.json (name, version), cleaned up App.js with adding SafeAreaView and StatusBar

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+	StatusBar,
+	Platform,
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
+
+console.log(StatusBar.currentHeight);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<SafeAreaView
+				style={{ flex: 1, marginTop: StatusBar.currentHeight }}
+			>
+				<View style={{ padding: 16, backgroundColor: "green" }}>
+					<Text>search</Text>
+				</View>
+				<View style={{ padding: 16, flex: 1, backgroundColor: "blue" }}>
+					<Text>list</Text>
+				</View>
+			</SafeAreaView>
+			<ExpoStatusBar style="auto" />
+		</>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
