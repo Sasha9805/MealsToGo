ef1ac9a067a2e3e7d75c6eff08205ec56bfbfb8f
Sasha Pimenov <pimenovs33@gmail.com>
2025-01-15 18:50:22 +0200

    Added styled-components and theme, added Restaurants feature

--- App.js
+++ App.js
@@ -1,36 +1,34 @@
 /* eslint-disable import/namespace */
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
+
+import {
+	useFonts as useOswald,
+	Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+	const [oswaldLoaded, oswaldError] = useOswald({
+		Oswald_400Regular,
+	});
+
+	const [latoLoaded, latoError] = useLato({
+		Lato_400Regular,
+	});
+
+	if (!oswaldLoaded || !latoLoaded) {
+		return null;
+	}
+
 	return (
 		<>
-			<SafeAreaView style={styles.container}>
-				<View style={styles.search}>
-					<Searchbar placeholder="Search" />
-				</View>
-				<View style={styles.list}>
-					<Text>list</Text>
-				</View>
-			</SafeAreaView>
-			<ExpoStatusBar style="auto" />
+			<ThemeProvider theme={theme}>
+				<RestaurantsScreen />
+				<ExpoStatusBar style="auto" />
+			</ThemeProvider>
 		</>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: StatusBar.currentHeight,
-	},
-	search: {
-		padding: 16,
-		backgroundColor: "green",
-	},
-	list: {
-		flex: 1,
-		padding: 16,
-		backgroundColor: "blue",
-	},
-});
---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,50 @@
+import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+
+const RestaurantCard = styled(Card)`
+	background-color: ${(props) => props.theme.colors.bg.primary};
+	border-radius: 0px;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+	padding: ${(props) => props.theme.space[3]};
+	background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Title = styled.Text`
+	color: ${(props) => props.theme.colors.ui.primary};
+	font-family: ${(props) => props.theme.fonts.heading};
+	font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const Address = styled.Text`
+	font-family: ${(props) => props.theme.fonts.body};
+	font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Info = styled.View`
+	padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+	const {
+		name = "Some Restaurant",
+		icon,
+		photos = [
+			"https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+		],
+		address = "100 some random street",
+		isOpenNow = true,
+		rating = 4,
+		isClosedTemporarily,
+	} = restaurant;
+	return (
+		<RestaurantCard elevation={5}>
+			<RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+			<Info>
+				<Title>{name}</Title>
+				<Address>{address}</Address>
+			</Info>
+		</RestaurantCard>
+	);
+};
