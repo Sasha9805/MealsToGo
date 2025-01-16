ca59effc1272f6e3485e4c8f4a37cf33e5b87a10
Sasha Pimenov <pimenovs33@gmail.com>
2025-01-16 19:46:49 +0200

    Added test list using FlatList

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,7 +1,8 @@
-import { StatusBar } from "react-native";
+import { FlatList, StatusBar } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SafeArea = styled.SafeAreaView`
 	flex: 1;
@@ -12,10 +13,11 @@ const SearchContainer = styled.View`
 	padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-	flex: 1;
-	padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+	contentContainerStyle: {
+		padding: 16,
+	},
+})``;
 
 export const RestaurantsScreen = () => {
 	return (
@@ -23,9 +25,15 @@ export const RestaurantsScreen = () => {
 			<SearchContainer>
 				<Searchbar placeholder="Search" theme={{ roundness: 0 }} />
 			</SearchContainer>
-			<RestaurantListContainer>
-				<RestaurantInfoCard />
-			</RestaurantListContainer>
+			<RestaurantList
+				data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+				renderItem={() => (
+					<Spacer position="bottom" size="large">
+						<RestaurantInfoCard />
+					</Spacer>
+				)}
+				keyExtractor={(item) => item.name}
+			/>
 		</SafeArea>
 	);
 };
